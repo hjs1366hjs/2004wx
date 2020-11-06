27e746350f9dacb34842de13646eb6aafe79ba9e
hjshjs <hjshjs1366@163.com>
2020-11-06 13:26:49 +0800

    接入token

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -7,4 +7,22 @@ use Illuminate\Http\Request;
 class WxController extends Controller
 {
     //
+   	public function AccessToken()
+   	{
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = env('WX_TOKEN');
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+   	}
 }
